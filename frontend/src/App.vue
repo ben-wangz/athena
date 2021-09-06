4173ef703466fb2fea94a89a897dff9330989545
ben.wangz <ben.wangz@foxmail.com>
2021-09-06 08:36:22 +0800

    first version

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <NesGame />
+  </div>
+</template>
+
+<script>
+import NesGame from './components/NesGame.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NesGame: NesGame
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
